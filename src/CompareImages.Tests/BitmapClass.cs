f036de029817b2521201eaabc4e623456694b888
Roman Kvasnyj <kvasnyj@gmail.com>
2018-04-11 15:42:34 +0200

    Basic implementation

---
+++ src/CompareImages.Tests/BitmapClass.cs
@@ -0,0 +1,60 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CompareImages;
+
+namespace CompareImages.Tests
+{
+    [TestClass]
+    public class BitmapClass
+    {
+        private static byte[] Bitmap2ByteArray(Bitmap image)
+        {
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+
+            var bitmapData = image.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadOnly, image.PixelFormat);
+            var length = bitmapData.Stride * bitmapData.Height;
+
+            byte[] bytes = new byte[length];
+
+            Marshal.Copy(bitmapData.Scan0, bytes, 0, length);
+            image.UnlockBits(bitmapData);
+
+            return bytes;
+        }
+
+        [TestMethod]
+        public void SameImages()
+        {
+            var img1 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "1img1.jpg")));
+            var img2 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "1img1.jpg")));
+
+            var distance = HistogramDistance.Calculate(img1, img2, false);
+
+            Assert.AreEqual(distance, 0);
+        }
+
+        [TestMethod]
+        public void SimilarImages()
+        {
+            var img1 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "1img1.jpg")));
+            var img2 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "1img2.jpg")));
+
+            var distance = HistogramDistance.Calculate(img1, img2, false);
+
+            Assert.IsTrue(distance < 10000);
+        }
+
+        [TestMethod]
+        public void DifferentImages()
+        {
+            var img1 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "1img1.jpg")));
+            var img2 = Bitmap2ByteArray(new Bitmap(Path.Combine("Images", "2img1.jpg")));
+
+            var distance = HistogramDistance.Calculate(img1, img2, false);
+
+            Assert.IsTrue(distance > 100000);
+        }
+    }
+}
---
+++ src/CompareImages/HistogramDistance.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace CompareImages
+{
+    public class HistogramDistance
+    {
+        private const int ColorQuantization = 8;
+
+        public static double Calculate(byte[] image1, byte[] image2, bool normalize)
+        {
+            var histogram1 = GetHistogram(image1, normalize);
+            var histogram2 = GetHistogram(image2, normalize);
+
+            var distance = 0d;
+            for (int i = 0; i < histogram1.Length; i++)
+            {
+                distance += Math.Abs(histogram1[i] - histogram2[i]);
+            }
+
+            return distance;
+        }
+
+        private static double[] GetHistogram(byte[] image, bool normalize)
+        {
+            var k = byte.MaxValue / ColorQuantization;
+            var size = image.Length;
+
+            var histogram = new double[k];
+
+            for (int i = 0; i < size; i++)
+            {
+                int quantColor = image[i] / k;
+
+                ++histogram[quantColor];
+            }
+
+            if (normalize)
+            {
+                for (int i = 0; i < ColorQuantization; i++)
+                {
+                    histogram[i] = histogram[i] / size;
+                }
+            }
+
+            return histogram;
+        }
+    }
+}
