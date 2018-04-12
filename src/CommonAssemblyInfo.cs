5837ecbe1e59f10e6a918fbc7e081ec232085da1
Roman Kvasnyj <kvasnyj@gmail.com>
2018-04-12 15:23:03 +0200

    Assembly version

---
+++ src/CommonAssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Albumprinter")]
+[assembly: AssemblyProduct("Albumprinter.CompareImages")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("Albumprinter")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
